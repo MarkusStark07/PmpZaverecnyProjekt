b67fc3319c7829a73b97b719325be51301e8cdd0
Marek Gajdoš <gajdosm.sk@gmail.com>
2026-06-01 09:03:50 +0200

    fixed microbit.py

--- microbit/microbit.py
+++ microbit/microbit.py
@@ -1,32 +1,29 @@
 from microbit import *
 import radio
 
-# 1. Nastavenie rádia
 radio.on()
 radio.config(group=10)
-# ZAPIS: Zapína rádiový modul a nastavuje "skupinu" 10.
-# Všetky micro:bity v projekte musia mať rovnaké číslo skupiny,
-# aby sa "počuli" navzájom.
-
-# 2. Inicializácia UART (Sériovej linky)
 uart.init(baudrate=115200)
-# ZAPIS: Pripravuje sériovú komunikáciu cez USB kábel.
-# Baudrate 115200 je štandardná rýchlosť, ktorou si PC a micro:bit
-# vymieňajú dáta.
+
+display.show(Image.ASLEEP)
 
 while True:
-    # 3. Čítanie z PC (UART)
+    # 1. Prijem z PC -> Rádio
     if uart.any():
-        data = uart.read()
-        radio.send_bytes(data)
-        display.show(Image.ARROW_N)
-        # ZAPIS: Ak PC pošle niečo cez USB, micro:bit to prečíta,
-        # pošle to vzduchom (rádiom) a rozsvieti šípku hore.
+        data_from_pc = uart.readline()
+        if data_from_pc:
+            msg = str(data_from_pc, 'utf-8').strip()
+            radio.send(msg)
+            # Vizuálna odozva
+            display.show(Image.ARROW_N)
+            sleep(200)
+            display.show(Image.ASLEEP)
 
-    # 4. Čítanie z rádia
-    incoming = radio.receive_bytes()
-    if incoming:
-        uart.write(incoming)
+    # 2. Príjem z Rádia -> PC
+    rcv_radio = radio.receive()
+    if rcv_radio:
+        uart.write(rcv_radio + '\n')
+        # Vizuálna odozva
         display.show(Image.ARROW_S)
-        # ZAPIS: Ak micro:bit zachytí rádio signál, pošle tieto dáta
-        # do PC cez USB a rozsvieti šípku dole.
+        sleep(200)
+        display.show(Image.ASLEEP),
